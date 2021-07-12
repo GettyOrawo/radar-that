fd051cc852a9f66a5ff097a5a5240772962bdf06
Getty Orawo <gettyorawo@gmail.com>
2021-07-12 15:30:44 +0300

    save each quadrant with location

--- lib/radar_detect/radar.ex
+++ lib/radar_detect/radar.ex
@@ -7,6 +7,7 @@ defmodule RadarDetect.Radar do
   alias RadarDetect.Repo
 
   alias RadarDetect.Radar.Matrix
+   alias RadarDetect.Radar.Quadrant
 
   @doc """
   Creates a matrix.
@@ -24,5 +25,54 @@ defmodule RadarDetect.Radar do
     %Matrix{}
     |> Matrix.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def load_matrix(%{x_axis_size: x_axis_size, input: input}) do
+    # {"x_axis_size": 6, "input": "5,5,6,7,8,9,3,3,4,5,6,0"}
+    height = string_to_list_of_ints(input) |> length() |> div(x_axis_size)
+
+    {:ok, matrix} = create_matrix(%{height: height, width: x_axis_size})
+    hii = load_quadrants(matrix, string_to_list_of_ints(input))
+    Quadrant
+    |> Repo.insert_all(hii)
+    
+  end
+
+  def load_quadrants(matrix, input) do
+    traverse(input, {0,0}, matrix, [])
+  end
+
+  def traverse([], {_x,y}, %{height: height}, acc) do
+    acc
+  end  
+
+  def traverse([h|t], {x,y}, %{width: width, id: id} = matrix, acc) when x < width do
+    new_acc = List.insert_at(acc, 0, %{x_axis: x, y_axis: y, value: h, matrix_id: id})
+    new_point = right({x,y})
+    traverse(t, new_point, matrix, new_acc)
+  end
+
+
+  def traverse(input, {x,y}, %{height: height, width: width} = matrix, acc) when x >= width do
+    new_point = down({0,y})
+    if y+1 <= height do
+      traverse(input, new_point, matrix, acc)
+    else
+      acc
+    end
+  end
+
+  defp right({x,y}) do
+    {x+1, y}
+  end
+
+  defp down({x,y}) do
+    {x, y+1}
+  end
+
+  defp string_to_list_of_ints(str) do
+    str
+    |> String.split(",") 
+    |> Enum.map(fn n -> Integer.parse(n) |> elem(0) end)
   end
 end
